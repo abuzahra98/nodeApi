3c2a616c931003236c2eecc88c7adcd4040e8447
“dawood-abuzahra” <dabuzahra@pando-soft.com>
2023-02-26 10:17:35 +0300

    fix

--- queries.js
+++ queries.js
@@ -2,49 +2,49 @@
 const { client } = require('./server');
 
 const getAll = async (tableName) => {
-    try {
-      const result = await client.query(`SELECT * FROM ${tableName}`);
-      return result.rows;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
+  try {
+    const result = await client.query(`SELECT * FROM ${tableName}`);
+    return result.rows;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-  const getById = async (auther_id,tableName) => {
-    try {
+const getById = async (auther_id, tableName) => {
+  try {
     //   const result = await client.query(`SELECT * FROM ${tableName}`);
-      const result = await client.query(`
+    const result = await client.query(`
       SELECT * FROM ${tableName} WHERE auther_id = $1
        `, [auther_id]);
-      return result.rows;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
-  
-  const insertRow = async (title, image,tableName) => {
-    try {
-      await client.query(`
+    return result.rows;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const insertRow = async (title, image, tableName) => {
+  try {
+    await client.query(`
         INSERT INTO ${tableName} (title, image)
         VALUES ($1, $2);
       `, [title, image]);
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
-  
-  const deleteRow = async (bookId,tableName) => {
-    try {
-      await client.query(`DELETE FROM ${tableName} WHERE id = $1`, [bookId]);
-      console.log(bookId, 'deleted');
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
-  
-  
-  module.exports = { getAll, insertRow, deleteRow ,getById};
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const deleteRow = async (bookId, tableName) => {
+  try {
+    await client.query(`DELETE FROM ${tableName} WHERE id = $1`, [bookId]);
+    console.log(bookId, 'deleted');
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+
+module.exports = { getAll, insertRow, deleteRow, getById };
